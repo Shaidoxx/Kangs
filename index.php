75fb74d08397bc7768030f25574d4149ba050bc7
Shaidoxx <67799866+Shaidoxx@users.noreply.github.com>
2022-11-16 17:34:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,102 +1,55 @@
-<?php session_start(); 
-
-
-    setcookie('pseudo', 'Shaidox_');
-    setcookie('id', 18);
-
-    if(isset($_COOKIE['age']))
-    {
-        echo "l'element existe" . $_COOKIE['pseudo'];
-    }
-    else
-    {
-        echo "l'element existe pas";
-    }
-
-    echo "id: " . $_COOKIE['id'];
-
-
-
-
-    // delete
-    // setcookie('pseudo', '');
-    // var_dump($_COOKIE);
-
-?>
-
-
 <!DOCTYPE html>
 <html>
 <head>
-<title>Kangs</title>
+	<title>Kangs - Accueil</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
 <body>
 
-    <h1>Bienvenue sur votre profil</h1>
-    <?php 
-    
-        if(isset($_SESSION['email']) && (isset($_SESSION['date'])))
-        {
-            ?>
-            
-            <p>Votre Email : <?= $_SESSION['email']; ?> </p>
-            <p>Votre Date : <?= $_SESSION['date']; ?> </p>
 
-            <?php
+	<!-- HAUT DE PAGE -->
+	<header>
+		<img src="">
+		<p>
+			<!-- Découvrez Kangs -->
+		</p>
+	</header>
 
-        }else{
-            echo "Veuillez vous connecter à votre compte";
-        }
-    
-    
-    
-    
-    ?>
-           
-           
+	<!-- Menu de Navigation -->
+	<nav class="menu-nav">
+		<ul>
+			<li class="btn">
+				<a href="#">
+					ACCUEIL
+				</a>
+			</li>
+			<li class="btn2">
+				<a href="boutique.html">
+					BOUTIQUE
+					&#x1F6D2
+				</a> 
+			</li>
+			<button>
+				PROFIL
+			  </button>
+			<li class="btn2">
+				<a href="profil.html">
+			</li>
+			<li class="btn">
+				<a href="social.html">
+					SOCIAL
+				</a>
+			</li>
+			
+		</ul>
+	</nav>
 
+	<!-- Contenue sous forme de boîte -->
 
+	
 
-
-
-    <!-- MENU NAV -->
-    <?php include 'menu-navigation.php'; 
-    
-    include 'includes/database.php';
-    global $db;
-    
-    ?>
-
-    
-
-
-    <h1>Connection</h1>
-    <form method="post">
-        <input type="text" name="lemail" id="lemail" placeholder="email" required><br/>
-        <input type="password" name="lpassword" id="lpassword" placeholder="Mot de passe" required><br/>
-        <input type="submit" name="formlogin" id="formlogin" value="Connection">
-</form>
-    
-
-    
-    
-<?php include 'includes/login.php'; ?>
-    <h1>S'inscrire</h1>
-    <form method="post">
-        <input type="text" name="email" id="email" placeholder="email" required><br/>
-        <input type="password" name="password" id="password" placeholder="Mot de passe" required><br/>
-        <input type="password" name="cpassword" id="cpassword" placeholder="Confirmer votre mot de passe" required><br/>
-        <input type="submit" name="formsend" id="formsend" value="S'inscrire">
-</form>
-
-        <?php include 'includes/signin.php'; ?>
-
-
-
-
-
-<!-- FOOTER & PARA -->
-<div id="content">
+	<div id="content">
 
 		<div class="leftbox"> 
 			
@@ -111,12 +64,28 @@
 		<div class="para2">
 			<p>
 				
-				Découvrez les offres de notre site
+				Découvrez notre site
 				<!-- <img src="img/logo.png"> -->
 			</p>
 		</div>
+		<!-- <div class="annonce">
+			<p>
+				
+				Salut3
+				<img src="img/logo.png">
+			</p>
+		</div> -->
+		
+		<!-- <div class="rightbox">
+			<h1>Dernière vidéo</h1>
 
-        <footer>
+			<iframe width="560" height="315" src="https://www.youtube.com/embed/bKuu5u7rFBI" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+		</div> -->
+	</div>
+
+	<!-- Footer -->
+
+	<footer>
 		<p>
 
 			Copyright &copy; Kangs - All Right reserved
